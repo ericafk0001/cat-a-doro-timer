dde114b3cc943a7628c939e0497620e21cd36bfa
ericafk0001 <dxslyofficial@gmail.com>
2024-12-29 16:33:25 -0500

    about section

--- script.js
+++ script.js
@@ -214,6 +214,21 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
   settingsCloseBtn.addEventListener("click", function () {
     settingsDiv.classList.remove("active");
+    const Toast = Swal.mixin({
+      toast: true,
+      position: "top-end",
+      showConfirmButton: false,
+      timer: 3000,
+      timerProgressBar: true,
+      didOpen: (toast) => {
+        toast.onmouseenter = Swal.stopTimer;
+        toast.onmouseleave = Swal.resumeTimer;
+      },
+    });
+    Toast.fire({
+      icon: "success",
+      title: "Settings Saved",
+    });
   });
 
   hamburger.addEventListener("click", function () {
